02e3ef9ebbdfbc2e134f61359e9dd831f9c1e2db
jraats <joost.de.man@gmail.com>
2015-06-13 16:01:27 +0200

    First GUI

---
+++ SearchAlgorithmParser/Gui/FrmMain.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Gui
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.regularExpressionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.regularGrammarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nDFAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dFAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 239);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.createToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // createToolStripMenuItem
+            // 
+            this.createToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.regularExpressionToolStripMenuItem,
+            this.regularGrammarToolStripMenuItem,
+            this.nDFAToolStripMenuItem,
+            this.dFAToolStripMenuItem});
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.createToolStripMenuItem.Text = "Create";
+            // 
+            // regularExpressionToolStripMenuItem
+            // 
+            this.regularExpressionToolStripMenuItem.Name = "regularExpressionToolStripMenuItem";
+            this.regularExpressionToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.regularExpressionToolStripMenuItem.Text = "Regular expression";
+            this.regularExpressionToolStripMenuItem.Click += new System.EventHandler(this.regularExpressionToolStripMenuItem_Click);
+            // 
+            // regularGrammarToolStripMenuItem
+            // 
+            this.regularGrammarToolStripMenuItem.Name = "regularGrammarToolStripMenuItem";
+            this.regularGrammarToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.regularGrammarToolStripMenuItem.Text = "Regular grammar";
+            this.regularGrammarToolStripMenuItem.Click += new System.EventHandler(this.regularGrammarToolStripMenuItem_Click);
+            // 
+            // nDFAToolStripMenuItem
+            // 
+            this.nDFAToolStripMenuItem.Name = "nDFAToolStripMenuItem";
+            this.nDFAToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.nDFAToolStripMenuItem.Text = "NDFA";
+            this.nDFAToolStripMenuItem.Click += new System.EventHandler(this.nDFAToolStripMenuItem_Click);
+            // 
+            // dFAToolStripMenuItem
+            // 
+            this.dFAToolStripMenuItem.Name = "dFAToolStripMenuItem";
+            this.dFAToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.dFAToolStripMenuItem.Text = "DFA";
+            this.dFAToolStripMenuItem.Click += new System.EventHandler(this.dFAToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMain";
+            this.Text = "Search algorithm parser";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem regularExpressionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem regularGrammarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nDFAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dFAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
